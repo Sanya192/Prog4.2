7c6d3fda5e1fa830d41055b561824d19dec7cbfa
Sanyi <major.sanyi@outlook.com>
2021-02-18 12:53:26 +0100

    Renamed AlphabetTextbox cause it's a number textbox. Watch when you copy from stackoverflow :D

--- Prog4.2/NumbersTextbox.cs
+++ Prog4.2/NumbersTextbox.cs
@@ -8,9 +8,13 @@ using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Prog4._2 {
-    public class AlphabetTextBox : TextBox {
+    public class NumbersTextbox : TextBox {
         private static readonly Regex regex = new Regex("^[0-9,-]+$");
 
+        /// <summary>
+        /// If the text is other than what Regex is allows then the event won't proceed.
+        /// </summary>
+        /// <param name="e">The input arguments.</param>
         protected override void OnPreviewTextInput(TextCompositionEventArgs e) {
             if (!regex.IsMatch(e.Text))
                 e.Handled = true;
